1f804098589818bdf32f1b228b140b69cc5de5c5
Jeff Karr <jeff.karr@digitalallyinc.com>
2021-07-18 17:17:46 -0500

    added calls to apis

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Articles msg="Welcome to Netlify Vue Test Page"/>
+    <h3>{{question}}</h3>
+    <h3>{{answer}}</h3>
   </div>
 </template>
 
@@ -13,11 +15,30 @@ export default {
   components: {
     Articles
   },
-  data() {
-    // articles: []
+  data () {
+    return {
+      question: '',
+      answer: ''
+      // articles: []
+    }
   },
   created: function () {
     // this.message = await 
+    let self = this;
+    fetch('/api/question')
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      self.question = data.question;
+    });
+
+    fetch('/api/answer')
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      self.answer = data.answer;
+    });
+
   }
 }
 </script>
